13e8dd25f1cbe9986e6c9ebfa97bb1d8c9371a14
josers77j <josers77.1@gmail.com>
2025-12-01 13:02:17 -0600

    fix: environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production:true,
+  companyName: ' Gifs',
+  companyName2: 'App',
+  companySlogan: 'The best gifs on the web',
+
+  /* api key */
+  giphyApiKey: 'BAeCGMOHBaQKYD9gMkZJMHJ0Vm0OvqpN',
+  giphyUrl: 'https://api.giphy.com/v1',
+};
